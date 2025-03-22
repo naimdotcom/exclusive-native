2892a63d8b03ffaf0ffdd9fc7885bf900b4d682b
Naim AK <149870242+naimak659@users.noreply.github.com>
2025-03-23 04:02:29 +0600

    Refactor Registration component to manage input state with useState; add onChangeText handlers for better data handling

--- src/components/Regisstration/Registration.tsx
+++ src/components/Regisstration/Registration.tsx
@@ -1,60 +1,82 @@
 import {ImageBackground, Text, TextInput, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import {RegistrationStyles} from './style';
 import {cf} from '../../styles/global';
 const bgImage = require('../../assets/someImage.jpg');
+
+export type signup = {
+  name: string;
+  email: string;
+  password: string;
+};
+
 const Registration = () => {
+  const [signupInfo, setSignupInfo] = useState<signup>({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handlChangeText = (e: any, id: string) => {
+    setSignupInfo({
+      ...signupInfo,
+      [id]: e,
+    });
+  };
+
   return (
     <>
-      <>
-        <View>
-          <ImageBackground
-            source={bgImage}
-            resizeMode="cover"
-            style={{
-              ...RegistrationStyles.image,
-            }}>
-            <View style={RegistrationStyles.overlay}></View>
+      <View>
+        <ImageBackground
+          source={bgImage}
+          resizeMode="cover"
+          style={{
+            ...RegistrationStyles.image,
+          }}>
+          <View style={RegistrationStyles.overlay}></View>
 
-            {/* content card */}
-            <View style={RegistrationStyles.card}>
-              <Text style={RegistrationStyles.title}>Create an Account</Text>
+          {/* content card */}
+          <View style={RegistrationStyles.card}>
+            <Text style={RegistrationStyles.title}>Create an Account</Text>
 
-              <View>
-                <Text style={RegistrationStyles.text}>Enter your Name</Text>
-                <TextInput
-                  placeholder="Name"
-                  keyboardType="default"
-                  style={cf(RegistrationStyles.textInput)}
-                  placeholderTextColor={'#AAA'}
-                />
-              </View>
-              <View>
-                <Text style={RegistrationStyles.text}>
-                  Enter your email address
-                </Text>
-                <TextInput
-                  placeholder="Email"
-                  keyboardType="email-address"
-                  style={cf(RegistrationStyles.textInput)}
-                  placeholderTextColor={'#AAA'}
-                />
-              </View>
-              <View>
-                <Text style={RegistrationStyles.text}>Enter Password</Text>
-                <TextInput
-                  placeholder="Password"
-                  keyboardType="default"
-                  style={cf(RegistrationStyles.textInput)}
-                  placeholderTextColor={'#AAA'}
-                  secureTextEntry={true}
-                />
-              </View>
+            <View>
+              <Text style={RegistrationStyles.text}>Enter your Name</Text>
+              <TextInput
+                placeholder="Name"
+                keyboardType="default"
+                style={cf(RegistrationStyles.textInput)}
+                placeholderTextColor={'#AAA'}
+                id="name"
+                onChangeText={e => handlChangeText(e, 'name')}
+              />
             </View>
-          </ImageBackground>
-        </View>
-      </>
+            <View>
+              <Text style={RegistrationStyles.text}>
+                Enter your email address
+              </Text>
+              <TextInput
+                placeholder="Email"
+                keyboardType="email-address"
+                style={cf(RegistrationStyles.textInput)}
+                placeholderTextColor={'#AAA'}
+                onChangeText={e => handlChangeText(e, 'email')}
+              />
+            </View>
+            <View>
+              <Text style={RegistrationStyles.text}>Enter Password</Text>
+              <TextInput
+                placeholder="Password"
+                keyboardType="default"
+                style={cf(RegistrationStyles.textInput)}
+                placeholderTextColor={'#AAA'}
+                secureTextEntry={true}
+                onChangeText={e => handlChangeText(e, 'password')}
+              />
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
     </>
   );
 };
